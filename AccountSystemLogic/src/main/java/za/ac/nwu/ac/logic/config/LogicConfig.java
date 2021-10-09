a7d4f475d720f0cc236f90dbd06ea83650e7911f
MrNene21 <91840995+MrNene21@users.noreply.github.com>
2021-10-09 16:50:25 +0200

    AccountSystemRepository
    added dependencies

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import za.ac.nwu.ac.Translator.config.TranslatorConfig;
+import za.ac.nwu.ac.translator.config.TranslatorConfig;
 
 @Import({TranslatorConfig.class})
 @Configuration
